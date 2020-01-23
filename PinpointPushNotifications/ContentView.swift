18d9c456e224e3812ba2a9cb9459f9ee5fb4d3b8
Michael Law <1365977+lawmicha@users.noreply.github.com>
2020-01-22 16:21:06 -0800

    initial project

---
+++ PinpointPushNotifications/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PinpointPushNotifications
+//
+//  Created by Law, Michael on 1/22/20.
+//  Copyright © 2020 Lawmicha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
